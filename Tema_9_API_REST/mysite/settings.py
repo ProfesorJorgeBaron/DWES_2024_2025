43a3e756ddc1dedfa4b335b2c9d26e1d6efa28de
Jorge Baron <jorge.baron@iespoligonosur.org>
2025-01-21 10:56:08 +0100

    Cambiar version oaut2

--- Tema_9_API_REST/mysite/settings.py
+++ Tema_9_API_REST/mysite/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
     ),
 }
 
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -151,7 +152,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
